63f164f9f0e6d45f078c7807dd9f42af8a0ca501
Tanoo Joyekurun <tanoo_joy@hotmail.com>
2025-05-17 18:21:48 +0400

    added riddle

--- lazy-grades/pages/404.js
+++ lazy-grades/pages/404.js
@@ -3,8 +3,7 @@ export default function Custom404() {
     return (
       <div className="min-h-screen flex flex-col items-center justify-center text-center p-6 bg-white text-black dark:bg-gray-900 dark:text-white">
         <h1 className="text-4xl font-bold mb-4">Oops! Page not found 😢</h1>
-        <p className="text-lg mb-6">You seem lost... or maybe You're not.</p>
-        <p className="text-lg mb-6">If you aren't lost, try another subdomain. Cmon, I'm a student trying stuff out what could possibly be a subdomain i created?</p>
+        <p className="text-lg mb-6">You seem lost... or maybe You're not. Maybe its riddle.</p>
         <a href="/" className="text-indigo-600 underline hover:text-indigo-800">← Go home</a>
       </div>
     );
